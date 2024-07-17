775f59ebf1ee9c1f1842cee4a3f5ef9a608eee2d
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-07-17 22:56:11 +0530

    add: data of DesignDummyData

---
+++ src/utils.js
@@ -0,0 +1,186 @@
+const DesignDummyData = [
+    {
+        id: 1,
+        name: 'Mandala Art',
+        image: "https://i.ibb.co/vjD9smd/Adobe-Scan-17-Jul-2024-14.jpg",
+        description: 'Latest Design ',
+        price: 699.9,
+        rating: 4.5,
+        stockStatus: 'In Stock',
+        brand: 'PragyAni',
+        reviews: [
+            { user: "Alice", comment: "Great art, love the quality!", rating: 5 },
+            { user: "Bob", comment: "Good value for money.", rating: 4 }
+        ]
+    },
+    {
+        id: 2,
+        name: 'Mandala Art',
+        image: "https://i.ibb.co/DVVW9LG/Adobe-Scan-17-Jul-2024-15.jpg",
+        description: 'Latest Design ',
+        price: 599.9,
+        rating: 4.0,
+        stockStatus: 'In Stock',
+        brand: 'PragyAni',
+        reviews: [
+            { user: "Alice", comment: "Great art, love the quality!", rating: 5 },
+            { user: "Bob", comment: "Good value for money.", rating: 4 }
+        ]
+    },
+    {
+        id: 3,
+        name: 'Mandala Art',
+        image: "https://i.ibb.co/Q6BvTGk/Adobe-Scan-17-Jul-2024-1.jpg",
+        description: 'Latest Desgin',
+        price: 589.1,
+        rating: 4.6,
+        stockStatus: 'In Stock',
+        brand: 'PragyAni',
+        reviews: [
+            { user: "Alice", comment: "Great art, love the quality!", rating: 5 },
+            { user: "Bob", comment: "Good value for money.", rating: 4 }
+        ]
+    },
+    {
+        id: 4,
+        name: 'Mandala Art',
+        image: "https://i.ibb.co/f0WGJTS/Adobe-Scan-17-Jul-2024-8.jpg",
+        description: 'Latest Design',
+        price: 550.2,
+        rating: 4.9,
+        stockStatus: 'In Stock',
+        brand: 'PragyAni',
+        reviews: [
+            { user: "Alice", comment: "Great art, love the quality!", rating: 5 },
+            { user: "Bob", comment: "Good value for money.", rating: 4 }
+        ]
+    },
+    {
+        id: 5,
+        name: 'Mandala Art',
+        image: "https://i.ibb.co/QPt8yzv/Adobe-Scan-17-Jul-2024-13.jpg",
+        description: 'Latest Design',
+        price: 650.2,
+        rating: 4.9,
+        stockStatus: 'In Stock',
+        brand: 'PragyAni',
+        reviews: [
+            { user: "Alice", comment: "Great art, love the quality!", rating: 5 },
+            { user: "Bob", comment: "Good value for money.", rating: 4 }
+        ]
+    },
+    {
+        id: 6,
+        name: 'Mandala Art',
+        image: "https://i.ibb.co/yFN9S23/Adobe-Scan-17-Jul-2024-2.jpg",
+        description: 'Latest Design',
+        price: 600.2,
+        rating: 3.9,
+        stockStatus: 'In Stock',
+        brand: 'PragyAni',
+        reviews: [
+            { user: "Alice", comment: "Great art, love the quality!", rating: 5 },
+            { user: "Bob", comment: "Good value for money.", rating: 4 }
+        ]
+    },
+    {
+        id: 7,
+        name: 'Mandala Art',
+        image: "https://i.ibb.co/xCfCT00/Adobe-Scan-17-Jul-2024-12.jpg",
+        description: 'Latest Design',
+        price: 580.8,
+        rating: 5,
+        stockStatus: 'In Stock',
+        brand: 'PragyAni',
+        reviews: [
+            { user: "Alice", comment: "Great art, love the quality!", rating: 5 },
+            { user: "Bob", comment: "Good value for money.", rating: 4 }
+        ]
+    },
+    {
+        id: 8,
+        name: 'Mandala Art',
+        image: "https://i.ibb.co/YRX1Rqc/Adobe-Scan-17-Jul-2024-11.jpg",
+        description: 'Latest Design',
+        price: 350,
+        rating: 4.0,
+        stockStatus: 'In Stock',
+        brand: 'PragyAni',
+        reviews: [
+            { user: "Alice", comment: "Great art, love the quality!", rating: 5 },
+            { user: "Bob", comment: "Good value for money.", rating: 4 }
+        ]
+    },
+    {
+        id: 9,
+        name: 'Mandala Art',
+        image: "https://i.ibb.co/dLP4swS/Adobe-Scan-17-Jul-2024-6.jpg",
+        description: 'Latest Design',
+        price: 680,
+        rating: 5.0,
+        stockStatus: 'In Stock',
+        brand: 'PragyAni',
+        reviews: [
+            { user: "Alice", comment: "Great art, love the quality!", rating: 5 },
+            { user: "Bob", comment: "Good value for money.", rating: 4 }
+        ]
+    },
+    {
+        id: 10,
+        name: 'Mandala Art',
+        image: "https://i.ibb.co/Bcp5zST/Adobe-Scan-17-Jul-2024-7.jpg",
+        description: 'Latest Design',
+        price: 570,
+        rating: 4.5,
+        stockStatus: 'In Stock',
+        brand: 'PragyAni',
+        reviews: [
+            { user: "Alice", comment: "Great art, love the quality!", rating: 5 },
+            { user: "Bob", comment: "Good value for money.", rating: 4 }
+        ]
+    },
+    {
+        id: 11,
+        name: 'Mandala Art',
+        image: "https://i.ibb.co/bBwX6p4/Adobe-Scan-17-Jul-2024-10.jpg",
+        description: 'Latest Design',
+        price: 580,
+        rating: 4.5,
+        stockStatus: 'In Stock',
+        brand: 'PragyAni',
+        reviews: [
+            { user: "Alice", comment: "Great art, love the quality!", rating: 5 },
+            { user: "Bob", comment: "Good value for money.", rating: 4 }
+        ]
+    },
+    {
+        id: 12,
+        name: 'Mandala Art',
+        image: "https://i.ibb.co/f0WGJTS/Adobe-Scan-17-Jul-2024-8.jpg",
+        description: 'Latest Design',
+        price: 500,
+        rating: 4.9,
+        stockStatus: 'In Stock',
+        brand: 'PragyAni',
+        reviews: [
+            { user: "Alice", comment: "Great art, love the quality!", rating: 5 },
+            { user: "Bob", comment: "Good value for money.", rating: 4 }
+        ]
+    },
+    {
+        id: 13,
+        name: 'Mandala Art',
+        image: "https://i.ibb.co/2cY0m3Z/Adobe-Scan-17-Jul-2024-9.jpg",
+        description: 'Latest Design',
+        price: 450,
+        rating: 4.9,
+        stockStatus: 'In Stock',
+        brand: 'PragyAni',
+        reviews: [
+            { user: "Alice", comment: "Great art, love the quality!", rating: 5 },
+            { user: "Bob", comment: "Good value for money.", rating: 4 }
+        ]
+    }
+
+]
+export default DesignDummyData;
