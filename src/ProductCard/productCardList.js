a2527efc4aa98d1e58464bd6652bbcb2cb1f1c9f
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-07-19 00:24:05 +0530

    add: productCardList.js

---
+++ src/ProductCard/productCardList.js
@@ -0,0 +1,18 @@
+ import React from "react";
+ import './productcardlist.css';
+
+ const ProductCardList=()=>{
+    return(
+        <>
+        <div className="item">
+            <div className="container-fluid">
+                 <div className="product-item">
+
+                 </div>
+            </div>
+        </div>
+        
+        </>
+    )
+ }
+ export  default ProductCardList ;
