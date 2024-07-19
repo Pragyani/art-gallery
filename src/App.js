bd1d0ee30a61487982fbd06c1cc0a6f291916aa4
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-07-19 16:13:54 +0530

    add : useState and useEffect for getting the data from utlies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import DesignDummyData from './utils';
+import NavBAr from './NavigationBar/navBar';
+import ProductCardList from './ProductCard/productCardList';
 
 function App() {
+
+  const [productData, setProductData] = useState([]);
+
+  useEffect(() => {
+    getProductData()
+  }, [])
+
+  const getProductData = async () => {
+    try {
+      setProductData(DesignDummyData)
+      // console.log('responseData', DesignDummyData)
+    } catch (error) {
+      console.log('this data is not working', error)
+    }
+
+  }
+
   return (
     <>
+      <NavBAr />
+      <ProductCardList data={productData} />
     </>
   );
 }
