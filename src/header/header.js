ae4fd102ee6b19bb26bc66140b381f91331ad003
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-07-21 21:26:18 +0530

    add: header.js file

---
+++ src/header/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './header.css'
+
+const Header = () => {
+    return (
+        <>
+            <div className="header-container">
+                <div className="content-header">
+                    <div className="heading-image">
+                        <img src="https://i.pinimg.com/564x/8e/2d/4e/8e2d4e56ae57f186014f5e9d82eec9e0.jpg" alt="image" />
+                        <h1>Let The Magic of Art <br/> Breathe New Life <br/> Into You</h1>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+export default Header;
