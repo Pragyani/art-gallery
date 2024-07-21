8e61e8ea016e76c246ca4e9c087a85f05f937361
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-07-21 14:02:57 +0530

    add: structure of productDetail

---
+++ src/ProductDetails/productDetail.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './productDetail.css';
+
+const ProductDetail = () => {
+    return (
+        <>
+            <div className="Container">
+                <div className="container-block">
+                    <div className="container-box">
+                        <div className="detailed-content">
+
+                            <div className="detail-p1">
+                                <div className="prduct-detImg">
+                                    <img src="https://i.ibb.co/DVVW9LG/Adobe-Scan-17-Jul-2024-15.jpg" alt="img" />
+                                </div>
+                            </div>
+
+                            <div className="detail-p2">
+                                <h1>Mandala Art</h1>
+                                <p>choose your collective favorite desgin by Pragya</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br/>  <br/>  <br/>  <br/>  <br/>
+        </>
+    )
+}
+export default ProductDetail;
