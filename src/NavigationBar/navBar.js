4e45ec6a73cd394583e24f4f4db646c0b6710ca3
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-07-26 14:15:15 +0530

    add : Link to navigation list

--- src/NavigationBar/navBar.js
+++ src/NavigationBar/navBar.js
@@ -4,7 +4,7 @@ import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import HdrStrongRoundedIcon from '@mui/icons-material/HdrStrongRounded';
 import CallRoundedIcon from '@mui/icons-material/CallRounded';
 import SettingsSuggestRoundedIcon from '@mui/icons-material/SettingsSuggestRounded';
-import Header from "../header/header";
+import { Link } from "react-router-dom";
 
 const NavBAr = () => {
     return (
@@ -16,18 +16,17 @@ const NavBAr = () => {
                     </div>
                     <div className="nav-p2">
                         <ul className="nav-ul">
-                            <li className="ul-li"><HdrStrongRoundedIcon className="material-icons"/>About us</li>
-                            <li className="ul-li"><HomeRoundedIcon className="material-icons"/>Home</li>
-                            <li className="ul-li"><CallRoundedIcon className="material-icons"/>Contact Us</li>
-                            <li className="ul-li"><SettingsSuggestRoundedIcon className="material-icons"/>Setting</li>
+                            <Link to={'/about'}><li className="ul-li"><HdrStrongRoundedIcon className="material-icons" />About us</li></Link>
+                            <Link to={'/'}><li className="ul-li"><HomeRoundedIcon className="material-icons" />Home</li></Link>
+                            <li className="ul-li"><CallRoundedIcon className="material-icons" />Contact Us</li>
+                            <li className="ul-li"><SettingsSuggestRoundedIcon className="material-icons" />Setting</li>
 
                         </ul>
                     </div>
                 </div>
             </div>
 
-            <hr/>
-            <Header/>
+            <hr />
         </>
     )
 }
