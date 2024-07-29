e8089ff7ea5a242538552eea8cfad069222a57cf
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-07-29 22:30:00 +0530

    add: userAccount Page.jsx

---
+++ src/pages/semiPages/UserAcountPage/AccountPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './accountsPage.css';
+
+const AccountPage = () =>{
+    return(
+        <>
+        <div className="slick-container">
+            <div className="slick-content">
+                <div className="account-section">
+                    <div className="page-image">
+                        <img src="https://i.pinimg.com/564x/bc/ab/f6/bcabf6462801ad74930edc36f043d4c0.jpg" alt="" />
+                     
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+export default AccountPage;
