1acd4f30ebc8296af150c826841b32d0fe606223
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-07-29 16:06:35 +0530

    add: SideBar and ProductDetail in Home.js

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,10 +1,15 @@
 import React from "react";
-import Header from "../../header/header";
+import './home.css';
+import ProductDetail from "../../ProductDetails/productDetail";
+import SideBar from "../../NavigationBar/NavigationSideBar/sideBar";
 
-const Home = () => {
+const Home = ({ data }) => {
     return (
         <>
-            <Header />
+            <div className="home-page">
+                <SideBar />
+                <ProductDetail Products={data} />
+            </div>
         </>
     )
 }
