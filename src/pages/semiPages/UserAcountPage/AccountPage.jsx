e5e78f0082a68e7376e419dfb3c17fa883488c84
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-07-30 15:53:54 +0530

    add: styling of input feild

--- src/pages/semiPages/UserAcountPage/AccountPage.jsx
+++ src/pages/semiPages/UserAcountPage/AccountPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './accountsPage.css';
+import Button from '@mui/material/Button';
 
 const AccountPage = () => {
     return (
@@ -17,6 +18,34 @@ const AccountPage = () => {
                                     <div className="text-flied">
                                         <h3>Welcome to the Art.Dribles Team</h3>
                                         <h4>Please login to your account</h4>
+                                    </div>
+
+                                    <section className="input-section">
+                                        <form className="form-field">
+                                            <input placeholder="user name or phone number" type="text" className="user-input-col" />
+                                            <br />
+                                            <input placeholder=" enter your  password" type="password" className="user-input-col" />
+                                        </form>
+                                    </section>
+
+                                    <div className="btn-section">
+                                        <Button className="button">Login</Button>
+                                    </div>
+
+                                    <div className="reset-password">
+                                        <h5>Forgot password ?</h5>
+                                    </div>
+
+                                    <div className="sign-up-box">
+                                        <div className="col-flexbox">
+                                            <h4>Don't have an account ?</h4>
+
+                                            <div className="bttn-bar">
+                                                <span className="btn">
+                                                    <Button className="buton">Create New Account</Button>
+                                                </span>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
