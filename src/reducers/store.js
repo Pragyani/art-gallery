b6ca52b435afbf77d8a7d89e6718ef4519a72464
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-08-29 15:27:25 +0530

    add : align the code

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import SavedPostReducer  from "./indx";
+import SavedPostReducer from "./indx";
 
 const store = configureStore({
-    reducer : {
-        savedProduct : SavedPostReducer
+    reducer: {
+        savedProduct: SavedPostReducer
     }
-})
+});
 
 export default store;
