63c14f778ba15973e32470293a6f381ed2c6df38
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-08-29 13:51:48 +0530

    add : action in reducers of indx.js

--- src/reducers/indx.js
+++ src/reducers/indx.js
@@ -1,2 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+export const SavedPostSlice = createSlice({
+    name:'savedPosts',
+    initialState:[],
+    reducers:{
+        addPosts : (state , action) =>{
+            if(!state.find(product => product.id === action.payload.id)){
+                return(state.push(action.payload))
+            }
+        }
+    }
+})  
+
+export const {addPosts} = SavedPostSlice.actions;
+export default SavedPostSlice.reducer;
