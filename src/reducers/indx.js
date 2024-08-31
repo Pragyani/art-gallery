c20a38331efc65d1359efc8cb8c8deaaa3fbab81
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-08-31 19:52:00 +0530

    add : addCartPost action in reducers

--- src/reducers/indx.js
+++ src/reducers/indx.js
@@ -1,16 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const SavedPostSlice = createSlice({
-    name:'savedPosts',
-    initialState:[],
-    reducers:{
-        addPosts : (state , action) =>{
-            if(!state.find(product => product.id === action.payload.id)){
-               state.push(action.payload)
+    name: 'savedPosts',
+    initialState: [],
+    reducers: {
+        addPosts: (state, action) => {
+            if (!state.find(product => product.id === action.payload.id)) {
+                state.push(action.payload)
+            }
+        },
+        addCartPost: (state, action) => {
+            const postExists = state.filter(product => product.id === action.payload.id);
+            if (postExists.length === 0) {
+                state.push(action.payload);
             }
         }
     }
-})  
+})
 
-export const {addPosts} = SavedPostSlice.actions;
+export const { addPosts, addCartPost } = SavedPostSlice.actions;
 export default SavedPostSlice.reducer;
