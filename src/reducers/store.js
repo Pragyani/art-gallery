6fc4f8b067a574d3e4b6dd38440657106b5ad078
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-08-31 20:08:09 +0530

    add : action (SavedCartReducer) in store

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import SavedPostReducer from "./indx";
+import SavedCartReducer from './indx'
 
 const store = configureStore({
     reducer: {
-        savedProduct: SavedPostReducer
+        savedProduct: SavedPostReducer,
+        cartedProducts : SavedCartReducer
     }
 });
 
