9a1d0a86e83ca54ffc08bac4cc8128b89f74f6c5
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-02 19:53:24 +0530

    add : icons in li's  and className  in sideBar.jsx

--- src/NavigationBar/NavigationSideBar/sideBar.jsx
+++ src/NavigationBar/NavigationSideBar/sideBar.jsx
@@ -4,7 +4,12 @@ import ArrowRightRoundedIcon from '@mui/icons-material/ArrowRightRounded';
 import { Link } from "react-router-dom";
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 import { Button } from "@mui/material";
+import ClassIcon from '@mui/icons-material/Class';
+import QuizIcon from '@mui/icons-material/Quiz';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useSelector } from "react-redux";
+import SignpostIcon from '@mui/icons-material/Signpost';
 
 const SideBar = () => {
 
@@ -17,23 +22,21 @@ const SideBar = () => {
                 <div className="content">
                     <div className="content-fluid">
                         <div className="sidebar">
-                            <h2 className="side-heading">Category</h2>
+                            <h2 className="side-heading">Your Category</h2>
 
                             <div className="category-list">
                                 <ul>
-                                    <li>Latest Post <ArrowRightRoundedIcon className="forward-icon" /></li>
-                                    <Link to={'./saved-post'}><li>Saved Post  <ArrowRightRoundedIcon className="forward-icon" /></li></Link>
+                                    <Link to={'/'}> <li><SignpostIcon className="user-prof-i" />Our Post <ArrowRightRoundedIcon className="forward-icon" /></li></Link>
+                                    <Link to={'./saved-post'}><li> <ClassIcon className="user-prof-i" />Saved Post  <ArrowRightRoundedIcon className="forward-icon" /></li></Link>
                                 </ul>
                             </div>
 
                             <div className="category-list">
                                 <ul>
-                                    <li>FAQ <ArrowRightRoundedIcon className="forward-icon" /></li>
-                                    <Link to={'/your-account'} ><li>Create Account <ArrowRightRoundedIcon className="forward-icon" /></li></Link>
-                                    <Link to={'/profilepage'}><li>Your Profile <AccountCircleRoundedIcon className="user-prof-i" /></li></Link>
-                                    <Link to={'/add-to-cart'}>
-                                        <li>Your Cart <Button>{cartItems.length}</Button></li>
-                                    </Link>
+                                    <li><QuizIcon className="user-prof-i" /> FAQ <ArrowRightRoundedIcon className="forward-icon" /></li>
+                                    <Link to={'/your-account'} ><li><PersonAddIcon className="user-prof-i" />Create Account <ArrowRightRoundedIcon className="forward-icon" /></li></Link>
+                                    <Link to={'/profilepage'}><li><AccountCircleRoundedIcon className="user-prof-i" />Your Profile</li></Link>
+                                    <Link to={'/add-to-cart'}><li><ShoppingCartIcon className="user-prof-i" />Your Cart <Button>{cartItems.length}</Button></li></Link>
                                 </ul>
                             </div>
                         </div>
