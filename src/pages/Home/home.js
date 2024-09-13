0a28cc20d4ef73f904b811f66b5886ada4b30f9a
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-13 17:33:56 +0530

    Deleting : SideBar from home.js

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,14 +1,11 @@
 import React from "react";
 import './home.css';
 import ProductDetail from "../../ProductDetails/productDetail";
-import SideBar from "../../NavigationBar/NavigationSideBar/sideBar";
 
 const Home = ({ data }) => {
     return (
         <>
             <div className="home-page">
-                <SideBar />
-                <hr className="hr-dep"/>
                 <ProductDetail Products={data} />
             </div>
         </>
