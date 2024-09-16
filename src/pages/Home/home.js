adf01e193385632953d2aa577ffa031d86beb4e7
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-16 15:57:18 +0530

    add : importing CommuintySideBar in  home.js file

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,12 +1,17 @@
 import React from "react";
 import './home.css';
 import ProductDetail from "../../ProductDetails/productDetail";
+import SideBar from "../../NavigationBar/NavigationSideBar/sideBar";
+import CommunitySideBar from "../../NavigationBar/NavigationSideBar/ComuunitySideBar/CommunitySidebar";
 
 const Home = ({ data }) => {
     return (
         <>
             <div className="home-page">
+                <SideBar />
+                <hr className="hr-dep" />
                 <ProductDetail Products={data} />
+                <CommunitySideBar />
             </div>
         </>
     )
