a9c35c8a2bb6cc8d6e0e25531c106f47c74391bf
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-17 21:27:57 +0530

    add : SideTopicBAr  and icons in SIdebar.jsx

--- src/NavigationBar/NavigationSideBar/sideBar.jsx
+++ src/NavigationBar/NavigationSideBar/sideBar.jsx
@@ -7,7 +7,10 @@ import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useSelector } from "react-redux";
 import SignpostIcon from '@mui/icons-material/Signpost';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import TurnedInRoundedIcon from '@mui/icons-material/TurnedInRounded';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import SideTopicBar from "./Sidetopicbar/SideTopicBar";
 
 const SideBar = () => {
 
@@ -21,12 +24,18 @@ const SideBar = () => {
                         <>
                             <div className="category-list">
                                 <ul>
-                                    <Link to={'/'}><li><SignpostIcon className="user-prof-i" /> Your Post</li> </Link>
-                                    <Link to={'./saved-post'}><li><ClassIcon className="user-prof-i" />Saved Post</li></Link>
+                                    <Link to={'/'}><li><HomeRoundedIcon className="user-prof-i" /> Home</li> </Link>
                                     <Link to={'/popular-page'}><li><TrendingUpIcon className="user-prof-i" />Popular Post</li></Link>
-                                    <Link to={'/your-account'}><li><PersonAddIcon className="user-prof-i" />Create Account</li></Link>
-                                    <Link to={'/profilepage'}><li><AccountCircleRoundedIcon className="user-prof-i" />Your Profile</li></Link>
-                                    <Link to={'/add-to-cart'}><li><ShoppingCartIcon className="user-prof-i" />Your Cart<button className="crt-btn">{cartItems.length}</button></li></Link>
+                                    <hr />
+                                    <div className="matrl-list">
+                                        <Link to={'./saved-post'}><li><TurnedInRoundedIcon className="user-prof-i" />Saved Post</li></Link>
+                                        <Link to={'/your-account'}><li><PersonAddIcon className="user-prof-i" />Create Account</li></Link>
+                                        <Link to={'/profilepage'}><li><AccountCircleRoundedIcon className="user-prof-i" />Your Profile</li></Link>
+                                        <Link to={'/add-to-cart'}><li><ShoppingCartIcon className="user-prof-i" />Your Cart<button className="crt-btn">{cartItems.length}</button></li></Link>
+                                    </div>
+                                    <hr />
+
+                                    <SideTopicBar/>
                                 </ul>
                             </div>
                         </>
