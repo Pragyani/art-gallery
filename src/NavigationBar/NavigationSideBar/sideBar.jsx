58766933427180013752f17a9acdfb5dfcf3c506
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-18 22:52:05 +0530

    add : change in link of savedPost

--- src/NavigationBar/NavigationSideBar/sideBar.jsx
+++ src/NavigationBar/NavigationSideBar/sideBar.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
 import './sidebar.css';
 import { Link } from "react-router-dom";
-import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
-import ClassIcon from '@mui/icons-material/Class';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useSelector } from "react-redux";
-import SignpostIcon from '@mui/icons-material/Signpost';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import TurnedInRoundedIcon from '@mui/icons-material/TurnedInRounded';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
@@ -28,14 +25,13 @@ const SideBar = () => {
                                     <Link to={'/popular-page'}><li><TrendingUpIcon className="user-prof-i" />Popular Post</li></Link>
                                     <hr />
                                     <div className="matrl-list">
-                                        <Link to={'./saved-post'}><li><TurnedInRoundedIcon className="user-prof-i" />Saved Post</li></Link>
+                                        <Link to={'/saved-post'}><li><TurnedInRoundedIcon className="user-prof-i" />Saved Post</li></Link>
                                         <Link to={'/your-account'}><li><PersonAddIcon className="user-prof-i" />Create Account</li></Link>
-                                        <Link to={'/profilepage'}><li><AccountCircleRoundedIcon className="user-prof-i" />Your Profile</li></Link>
                                         <Link to={'/add-to-cart'}><li><ShoppingCartIcon className="user-prof-i" />Your Cart<button className="crt-btn">{cartItems.length}</button></li></Link>
                                     </div>
                                     <hr />
 
-                                    <SideTopicBar/>
+                                    <SideTopicBar />
                                 </ul>
                             </div>
                         </>
