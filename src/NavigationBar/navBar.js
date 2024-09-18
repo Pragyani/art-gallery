62f188f62ce9cd6c21fe44d3cc624d6301df9c96
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-18 22:48:34 +0530

    Add : theme mode and userprofille image in navbar.jsx

--- src/NavigationBar/navBar.js
+++ src/NavigationBar/navBar.js
@@ -1,23 +1,18 @@
 import React from "react";
 import './nav.css';
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-import HdrStrongRoundedIcon from '@mui/icons-material/HdrStrongRounded';
-import CallRoundedIcon from '@mui/icons-material/CallRounded';
-import SettingsSuggestRoundedIcon from '@mui/icons-material/SettingsSuggestRounded';
 import { Link } from "react-router-dom";
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
-import SideBar from "./NavigationSideBar/sideBar";
 import { UseTheme } from "../theme-context/theme-context";
 
 const NavBAr = () => {
-    const {toogleTheme , theme} = UseTheme();
+    const { toogleTheme, theme } = UseTheme();
 
     return (
         <>
             <div className="NavigationBar">
                 <div className="navBar">
                     <div className="nav-p1">
-                        <h1>art.Gallery</h1>
+                        <Link to={'./'}><h1>art.Gallery</h1></Link>
                     </div>
 
                     <div className="input-field">
@@ -29,17 +24,19 @@ const NavBAr = () => {
 
                     <div className="nav-p2">
                         <ul className="nav-ul">
-                            <Link to={'/'}><li className="ul-li"><HomeRoundedIcon className="material-icons" />Home</li></Link>
-                            <Link to={'/about'}><li className="ul-li"><HdrStrongRoundedIcon className="material-icons" />About us</li></Link>
-                            <li className="ul-li">Theme Mode<input type="checkbox" onChange={toogleTheme} checked={theme === 'light'}/></li>
+                            <Link to={'/'}><li className="ul-li"> Your Post</li> </Link>
+                            <li className="ul-li">Default Mode<input type="checkbox" onChange={toogleTheme} checked={theme === 'light'} /></li>
+                            <li className="ul-l">
+                                <span className="li-span-img">
+                                    <Link to={'/profilepage'}> <img src="https://i.pinimg.com/564x/aa/b2/03/aab2036f11e01e7bc615488478e36568.jpg" /></Link>
+                                </span>
+                            </li>
 
                         </ul>
                     </div>
                 </div>
             </div>
-
             <hr />
-            <SideBar />
         </>
     )
 }
