50abd9c8d57e6b4f5e723f379571118c7368c2b9
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-18 20:55:02 +0530

    add : toggle effect to the THumbAltICons

--- src/ProductDetails/productDetail.js
+++ src/ProductDetails/productDetail.js
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
 import './productDetail.css';
-import Rating from '@mui/material/Rating';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import { useDispatch } from "react-redux";
-import { addPost, addCartProducts } from "../reducers/indx";
+import { addPost, addCartProducts, addPopularPost } from "../reducers/indx";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import PersonPinIcon from '@mui/icons-material/PersonPin';
-import FavoriteBorderSharpIcon from '@mui/icons-material/FavoriteBorderSharp';
 import ShareIcon from '@mui/icons-material/Share';
-import { BiUpvote } from "react-icons/bi";
-import { BiDownvote } from "react-icons/bi";
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 
 const ProductDetail = ({ Products }) => {
     const dispatch = useDispatch();
     const [bookmarkedProducts, setBookmarkedProducts] = useState({});
     const [cartedmarkedProducts, setCartedMarkedProduct] = useState({});
+    const [voteMarked, setvoteMarked] = useState({});
 
     const handleSaveProduct = (product) => {
         dispatch(addPost(product));
@@ -25,6 +24,10 @@ const ProductDetail = ({ Products }) => {
     const addToCarthandle = (product) => {
         dispatch(addCartProducts(product));
     };
+
+    const handleSavedPopularPosts = (product) => {
+        dispatch(addPopularPost(product));
+    }
 
     const handleBookmarkToggle = (product) => {
         setBookmarkedProducts((prev) => ({
@@ -53,6 +56,20 @@ const ProductDetail = ({ Products }) => {
             addToCarthandle(product);
         }
     }
+
+    const handleVoteMarkToggle = (product) => {
+        setvoteMarked((prev) => ({
+            ...prev,
+            [product.id]: !prev[product.id]
+        }));
+    };
+
+    const handlePopularPost = (product) => {
+        handleVoteMarkToggle(product);
+        if (!voteMarked[product.id]) {
+            handleSavedPopularPosts(product);
+        }
+    }
     return (
         <>
             <div className="detail-container">
@@ -68,12 +85,12 @@ const ProductDetail = ({ Products }) => {
                                                 <div>Your Post : <b><span>{item?.brand}</span></b></div>
                                             </div>
 
-
                                             <div className="product-box">
                                                 <div className="product-img">
                                                     <img src={item?.image} alt="Product" />
                                                 </div>
                                             </div>
+
                                             <div className="product-breif">
                                                 <div className="info-prod">
                                                     <div className="info-data">
@@ -84,32 +101,26 @@ const ProductDetail = ({ Products }) => {
                                                         </span>
                                                         <div className="post-icons">
                                                             <div className="liked-ico">
-                                                                <BiUpvote className="vote-bicon" />
-                                                                <BiDownvote className="vote-bicon" />
+                                                                <h3 onClick={() => handlePopularPost(item)} className="hrt-st">
+                                                                    {voteMarked[item.id] ? <ThumbUpAltIcon /> : <ThumbUpOffAltIcon />}
+                                                                </h3>
                                                             </div>
                                                             <ShareIcon className="hrt-st" />
                                                             <h3 onClick={() => handleBookmarkClick(item)} className="hrt-st">
                                                                 {bookmarkedProducts[item.id] ? <BookmarkIcon /> : <BookmarkBorderIcon />}
                                                             </h3>
+
                                                             <h3 onClick={() => CartedHandleMarkedClicked(item)} className="crt-st">
                                                                 {cartedmarkedProducts[item.id] ? <ShoppingCartIcon /> : <AddShoppingCartIcon />}
                                                             </h3>
                                                         </div>
-
                                                         <h4>Designed By Pragya</h4>
                                                         <h5 className="price-tag">Price - <span>{item?.price}</span></h5>
-                                                        {/* <h5>Rating - <Rating name="customized-10" defaultValue={item?.rating} max={5} className="rating-crd" /></h5> */}
                                                         <p>Stock Status - <h6>{item?.stockStatus}</h6></p>
-                                                        {/* <h5 className="name">{item.name}</h5> */}
-                                                        <span className="input-span-tag"><input placeholder="Add Comment ...." type="text"/></span>
-
+                                                        <span className="input-span-tag"><input placeholder="Add Comment ...." type="text" /></span>
                                                         {/* <h4>Brand Name - <b>{item?.brand}</b></h4> */}
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div className="saved-mark-product">
-
-
                                             </div>
                                         </div>
                                     )
