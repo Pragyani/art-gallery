0d0b6bb4ea6c70ca37511ef86041a18c0ed50f6a
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-23 21:47:01 +0530

    add : updation in  cl-web-add

--- src/pages/Footer/footer.jsx
+++ src/pages/Footer/footer.jsx
@@ -8,18 +8,18 @@ const Footer = () => {
             <section className="footer-section">
                 <div className="cl-foterr-class">
                     <div className="footer-slics">
-                       <Link to={'./'}> <h1>art.Gallery</h1></Link>
+                        <Link to={'./'}> <h1>art.Gallery</h1></Link>
 
-                       <div className="web-info">
-                        <span className="cl-web-add">
-                            126-C Green Avenue , Shalimar Bagh <br/>
-                            Delhi-110092.
-                        </span>
-                       </div>
+                        <div className="web-info">
+                            <span className="cl-web-add">
+                                126-C Green Avenue , Shalimar Bagh
+                                <br /> <span className="br-letter"> Delhi-110092.</span>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </section>
-<br /><br />
+            <br /><br />
         </>
     )
 }
