9d2e96acc23add9410eced1fc8d943618fbb3000
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-23 22:16:38 +0530

    add : contact no className and h3 in footer.jsx

--- src/pages/Footer/footer.jsx
+++ src/pages/Footer/footer.jsx
@@ -16,6 +16,13 @@ const Footer = () => {
                                 <br /> <span className="br-letter"> Delhi-110092.</span>
                             </span>
                         </div>
+
+                        <div className="contct-no">
+                            <div className="footer-wb-no">+11-34528357 , 98645792161</div>
+                            <div className="web-info-emails">Email id: <span className="id">@myartgmail.com</span></div>
+                        </div>
+
+                        <h3>Thanks for visit our  art.Gallery</h3>
                     </div>
                 </div>
             </section>
