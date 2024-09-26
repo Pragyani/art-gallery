20169238af383bf6334aafdc086b92ca4aa6a3e6
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-26 21:27:18 +0530

    add : TopBar.js

---
+++ src/pages/popularPostPage/PopularTopBar/TopBar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import './topbar.css';
+
+const TopBar = () => {
+    return (
+        <>
+            <div className="page-cl-content">
+                <div className="popular-page-item">
+                    <div className="item-page">
+                        <ul className="poplr-pages-content">
+                            <li>
+                                <img src="https://i.pinimg.com/564x/a0/ca/a4/a0caa4c1da70e2bd26c0d636cb42278a.jpg" alt="" />
+                            </li>
+
+                            <li>
+                                <img src="https://i.pinimg.com/564x/a0/ca/a4/a0caa4c1da70e2bd26c0d636cb42278a.jpg" alt="" />
+                            </li>
+                            <li>
+                                <img src="https://i.pinimg.com/564x/a0/ca/a4/a0caa4c1da70e2bd26c0d636cb42278a.jpg" alt="" />
+                            </li>
+                            <li>
+                                <img src="https://i.pinimg.com/564x/a0/ca/a4/a0caa4c1da70e2bd26c0d636cb42278a.jpg" alt="" />
+                            </li>
+                            <li>
+                                <img src="https://i.pinimg.com/564x/a0/ca/a4/a0caa4c1da70e2bd26c0d636cb42278a.jpg" alt="" />
+                            </li>
+                            <li>
+                                <img src="https://i.pinimg.com/564x/a0/ca/a4/a0caa4c1da70e2bd26c0d636cb42278a.jpg" alt="" />
+                            </li>
+                            <li>
+                                <img src="https://i.pinimg.com/564x/a0/ca/a4/a0caa4c1da70e2bd26c0d636cb42278a.jpg" alt="" />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default TopBar;
