a70640b9741c9d80a59161a9b0477601e59d0af8
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-26 20:37:48 +0530

    add : importing TopBar.jsx in PopularPst

--- src/pages/popularPostPage/PopularPost.jsx
+++ src/pages/popularPostPage/PopularPost.jsx
@@ -5,53 +5,55 @@ import { Link } from "react-router-dom";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import SideBar from "../../NavigationBar/NavigationSideBar/sideBar";
 import './popularpostpage.css'
+import TopBar from "./PopularTopBar/TopBar";
 
 const PopularPost = () => {
     const PostPopulars = useSelector((state) => state.popularPost);
     return (
         <>
-        <div className="sidesection">
-            
-    <SideBar/><hr />
-        <div className="popular-products-container">
-            <h1>Popular Posts</h1>
-            <div className="popular-products-list">
-                {PostPopulars.length === 0 ? (
-                    <p>No Post Are Popular yet! <br /> <span><Link to={'/'}>BACK TO HOME <ArrowForwardIosIcon className="arr" /></Link></span></p>
+            <div className="sidesection">
 
-                ) : (
-                    PostPopulars.map((product) => (
-                        <div className="fluid-post">
-                            <div className="re-container">
-                                <div className="product-fluid">
-                                    <div key={product.id} className="products">
-                                        <div className="product-box">
-                                            <div className="product-img">
-                                                <img src={product.image} alt="product" />
-                                            </div>
-                                        </div>
+                <SideBar /><hr />
+                <div className="popular-products-container">
+                    <h1>Popular Posts</h1>
+                    <TopBar />
+                    <div className="popular-products-list">
+                        {PostPopulars.length === 0 ? (
+                            <p>Latest Post Are Popular yet! <br /> <span><Link to={'/'}>BACK TO HOME <ArrowForwardIosIcon className="arr" /></Link></span></p>
 
-                                        <div className="product-breif">
-                                            <div className="info-prod">
-                                                <div className="info-data">
-                                                    <h4>Desgined By Pragya</h4>
-                                                    <h5 className="name">{product.name}</h5>
-                                                    <h5 className="price-tag">Price - <span>{product.price}</span></h5>
-                                                    <h5>Rating -<Rating name="customized-10" defaultValue={product.rating} max={5} className="rating-crd" /></h5>
-                                                    <p>StockStatus - <h6>{product.stockStatus}</h6> </p>
-                                                    <h4>Brand Name - <b>{product.brand}</b></h4>
+                        ) : (
+                            PostPopulars.map((product) => (
+                                <div className="fluid-post">
+                                    <div className="re-container">
+                                        <div className="product-fluid">
+                                            <div key={product.id} className="products">
+                                                <div className="product-box">
+                                                    <div className="product-img">
+                                                        <img src={product.image} alt="product" />
+                                                    </div>
+                                                </div>
+
+                                                <div className="product-breif">
+                                                    <div className="info-prod">
+                                                        <div className="info-data">
+                                                            <h4>Desgined By Pragya</h4>
+                                                            <h5 className="name">{product.name}</h5>
+                                                            <h5 className="price-tag">Price - <span>{product.price}</span></h5>
+                                                            <h5>Rating -<Rating name="customized-10" defaultValue={product.rating} max={5} className="rating-crd" /></h5>
+                                                            <p>StockStatus - <h6>{product.stockStatus}</h6> </p>
+                                                            <h4>Brand Name - <b>{product.brand}</b></h4>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    ))
-                )}
+                            ))
+                        )}
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
         </>
     );
 };
