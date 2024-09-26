07c95c2c0f275c49a87d37126a8d5c2dcf03e4a7
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-26 22:25:49 +0530

    add : image url's of TopBar

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ const DesignDummyData = [
         name: 'Mandala Art',
         image: "https://i.ibb.co/vjD9smd/Adobe-Scan-17-Jul-2024-14.jpg",
         description: 'Latest Design ',
-        price: 699.9,
+        price: 699,
         rating: 4,
         stockStatus: 'In Stock',
         brand: 'PragyAni',
@@ -18,7 +18,7 @@ const DesignDummyData = [
         name: 'Mandala Art',
         image: "https://i.ibb.co/DVVW9LG/Adobe-Scan-17-Jul-2024-15.jpg",
         description: 'Latest Design ',
-        price: 599.9,
+        price: 599,
         rating: 4,
         stockStatus: 'In Stock',
         brand: 'PragyAni',
@@ -32,7 +32,7 @@ const DesignDummyData = [
         name: 'Mandala Art',
         image: "https://i.ibb.co/Q6BvTGk/Adobe-Scan-17-Jul-2024-1.jpg",
         description: 'Latest Desgin',
-        price: 589.1,
+        price: 589,
         rating: 4,
         stockStatus: 'In Stock',
         brand: 'PragyAni',
@@ -46,7 +46,7 @@ const DesignDummyData = [
         name: 'Mandala Art',
         image: "https://i.ibb.co/f0WGJTS/Adobe-Scan-17-Jul-2024-8.jpg",
         description: 'Latest Design',
-        price: 550.2,
+        price: 550,
         rating: 4,
         stockStatus: 'In Stock',
         brand: 'PragyAni',
@@ -60,7 +60,7 @@ const DesignDummyData = [
         name: 'Mandala Art',
         image: "https://i.ibb.co/QPt8yzv/Adobe-Scan-17-Jul-2024-13.jpg",
         description: 'Latest Design',
-        price: 650.2,
+        price: 650,
         rating: 4,
         stockStatus: 'In Stock',
         brand: 'PragyAni',
@@ -74,7 +74,7 @@ const DesignDummyData = [
         name: 'Mandala Art',
         image: "https://i.ibb.co/yFN9S23/Adobe-Scan-17-Jul-2024-2.jpg",
         description: 'Latest Design',
-        price: 600.2,
+        price: 600,
         rating: 3,
         stockStatus: 'In Stock',
         brand: 'PragyAni',
@@ -88,7 +88,7 @@ const DesignDummyData = [
         name: 'Mandala Art',
         image: "https://i.ibb.co/xCfCT00/Adobe-Scan-17-Jul-2024-12.jpg",
         description: 'Latest Design',
-        price: 580.8,
+        price: 580,
         rating: 5,
         stockStatus: 'In Stock',
         brand: 'PragyAni',
@@ -170,3 +170,21 @@ const DesignDummyData = [
 
 ]
 export default DesignDummyData;
+
+
+
+//topbar image url's
+
+export const TopBarImage1 = "https://i.pinimg.com/564x/b7/95/57/b79557f6abce348236d43109ca7e4d8f.jpg";
+
+export const TopBarImage2 = "https://i.pinimg.com/564x/3a/80/e0/3a80e00eac156b567f7096f98b3c45b0.jpg";
+
+export const TopBarImage3 = "https://i.pinimg.com/564x/23/5b/61/235b616e260c0ddd19a9a59ff9283bf2.jpg";
+
+export const TopBarImage4 = "https://i.pinimg.com/564x/23/50/59/235059b09c5aa4165f27224bf154c6bc.jpg";
+
+export const TopBarImage5 = "https://i.pinimg.com/564x/89/88/7d/89887d794ec77f8cf48028d9fe1e37fe.jpg";
+
+export const TopBarImage6 = "https://i.pinimg.com/564x/e4/6d/49/e46d49c313d82c8d107893f37528b24d.jpg";
+
+export const TopBarImage7 = "https://i.pinimg.com/564x/83/0c/76/830c76e330d0a8eb207182abfecf6715.jpg";
