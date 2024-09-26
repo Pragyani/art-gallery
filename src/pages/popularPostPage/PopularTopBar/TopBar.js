ce66626223f1ddbc035f6f3a6de138182d2a17e5
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-09-26 22:14:52 +0530

    add : optimization in TopBar img

--- src/pages/popularPostPage/PopularTopBar/TopBar.js
+++ src/pages/popularPostPage/PopularTopBar/TopBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './topbar.css';
+import { TopBarImage1, TopBarImage2, TopBarImage3, TopBarImage4, TopBarImage5, TopBarImage6, TopBarImage7 } from "../../../utils";
 
 const TopBar = () => {
     return (
@@ -9,32 +10,32 @@ const TopBar = () => {
                     <div className="item-page">
                         <ul className="poplr-pages-content">
                             <li>
-                                <img src="https://i.pinimg.com/564x/b7/95/57/b79557f6abce348236d43109ca7e4d8f.jpg" alt="popul-page-imamges" />
+                                <img src={TopBarImage1} alt="popul-page-imamges" />
                                 <h4>Space Painting</h4>
                             </li>
 
                             <li>
-                                <img src="https://i.pinimg.com/564x/3a/80/e0/3a80e00eac156b567f7096f98b3c45b0.jpg" alt="popul-page-imamges" />
+                                <img src={TopBarImage2} alt="popul-page-imamges" />
                                 <h4>Mandala Art</h4>
                             </li>
                             <li>
-                                <img src="https://i.pinimg.com/564x/23/5b/61/235b616e260c0ddd19a9a59ff9283bf2.jpg" alt="popul-page-imamges" />
+                                <img src={TopBarImage3} alt="popul-page-imamges" />
                                 <h4>Canvas Painting</h4>
                             </li>
                             <li>
-                                <img src="https://i.pinimg.com/564x/23/50/59/235059b09c5aa4165f27224bf154c6bc.jpg" alt="popul-page-imamges" />
+                                <img src={TopBarImage4} alt="popul-page-imamges" />
                                 <h4>Designs</h4>
                             </li>
                             <li>
-                                <img src="https://i.pinimg.com/564x/89/88/7d/89887d794ec77f8cf48028d9fe1e37fe.jpg" alt="popul-page-imamges" />
+                                <img src={TopBarImage5} alt="popul-page-imamges" />
                                 <h4>Painting</h4>
                             </li>
                             <li>
-                                <img src="https://i.pinimg.com/564x/e4/6d/49/e46d49c313d82c8d107893f37528b24d.jpg" alt="popul-page-imamges" />
+                                <img src={TopBarImage6} alt="popul-page-imamges" />
                                 <h4>Doddle Art</h4>
                             </li>
                             <li>
-                                <img src="https://i.pinimg.com/564x/83/0c/76/830c76e330d0a8eb207182abfecf6715.jpg" alt="popul-page-imamges" />
+                                <img src={TopBarImage7} alt="popul-page-imamges" />
                                 <h4>Madhubani Art</h4>
                             </li>
                         </ul>
