e59f0896c53bc143f682dbee27dd0bf62ea926c0
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-10-03 18:40:45 +0530

    add : toggleCartProduct

--- src/reducers/indx.js
+++ src/reducers/indx.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// Saved Posts Slice
 export const SavedPostSlice = createSlice({
     name: 'savedPosts',
     initialState: [],
@@ -13,9 +14,11 @@ export const SavedPostSlice = createSlice({
             return state.filter(product => product.id !== action.payload.id);
         }
     }
-})
+});
+
 export const { addPost, removePost } = SavedPostSlice.actions;
 
+// Cart Products Slice
 export const cartProductsSlice = createSlice({
     name: 'cartProducts',
     initialState: [],
@@ -27,12 +30,21 @@ export const cartProductsSlice = createSlice({
         },
         removeCartProduct: (state, action) => {
             return state.filter(product => product.id !== action.payload.id);
+        },
+        toggleCartProduct: (state, action) => {
+            const existingProduct = state.find(product => product.id === action.payload.id);
+            if (existingProduct) {
+                return state.filter(product => product.id !== action.payload.id); 
+            } else {
+                state.push(action.payload);
+            }
         }
     }
-})
+});
 
-export const { addCartProducts, removeCartProduct } = cartProductsSlice.actions;
+export const { addCartProducts, removeCartProduct, toggleCartProduct } = cartProductsSlice.actions;
 
+// Popular Posts Slice
 export const popularPostSlice = createSlice({
     name: 'popularPost',
     initialState: [],
@@ -46,20 +58,22 @@ export const popularPostSlice = createSlice({
             return state.filter(product => product.id !== action.payload.id);
         }
     }
-})
+});
 
 export const { addPopularPost, removePopularPost } = popularPostSlice.actions;
 
+// Input Slice
 export const inputSliceIndex = createSlice({
     name: 'inputslice',
     initialState: '',
     reducers: {
         setInput: (state, action) => action.payload
     }
-})
+});
 
 export const { setInput } = inputSliceIndex.actions;
 
+// Export reducers
 export default {
     savedPostsReducer: SavedPostSlice.reducer,
     cartProductsReducer: cartProductsSlice.reducer,
