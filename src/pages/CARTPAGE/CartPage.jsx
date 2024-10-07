e77d7ae350c250337059107ac95d832baf576e85
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-10-07 15:55:59 +0530

    add : useEffect in CartPage to get the data in browserpage

--- src/pages/CARTPAGE/CartPage.jsx
+++ src/pages/CARTPAGE/CartPage.jsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './CartPage.css';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import Rating from '@mui/material/Rating';
 import SideBar from "../../NavigationBar/NavigationSideBar/sideBar";
+import { setCartProducts } from "../../reducers/indx";
 
 const CartPage = () => {
+    const dispatch = useDispatch();
     const cartProducts = useSelector((state) => state.cartProducts);
+
+    useEffect(() => {
+        const storedCart = localStorage.getItem('cartProducts');
+        if (storedCart) {
+            dispatch(setCartProducts(JSON.parse(storedCart)));
+        }
+    }, [dispatch]);
+
+    useEffect(() => {
+        localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
+    }, [cartProducts]);
 
     return (
         <div className="cart-products-container">
             <SideBar /> <hr className="cartpage-hr"/>
-
             <div className="cart-products-list">
-                
-            <h1>CART ITEMS</h1>
+                <h1>CART ITEMS</h1>
                 {cartProducts.length === 0 ? (
                     <p>No products in cart yet! <br /> <span><Link to={'/'}>BACK TO HOME <ArrowForwardIosIcon className="arr" /></Link></span></p>
                 ) : (
@@ -37,11 +48,13 @@ const CartPage = () => {
                                                     <h5 className="name">{product.name}</h5>
                                                     <h5 className="price-tag">Price - <span>{product.price}</span></h5>
                                                     <h5>Rating -<Rating name="customized-10" defaultValue={product.rating} max={5} className="rating-crd" /></h5>
-                                                    <p>Stock Status - <h6>{product.stockStatus}</h6> </p>
+                                                    <p>Stock Status - <h6>{product.stockStatus}</h6></p>
                                                     <h4>Brand Name - <b>{product.brand}</b></h4>
                                                 </div>
                                             </div>
                                         </div>
+
+                                     
                                     </div>
                                 </div>
                             </div>
