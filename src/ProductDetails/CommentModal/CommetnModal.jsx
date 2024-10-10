fb89288d2533c3ff3ea50af5c721661b4e40b115
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-10-10 19:42:10 +0530

    add : h1 elment

--- src/ProductDetails/CommentModal/CommetnModal.jsx
+++ src/ProductDetails/CommentModal/CommetnModal.jsx
@@ -8,11 +8,9 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 790,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
     boxShadow: 24,
-    p: 4,
 };
 
 export default function BasicCommentMODAL({ commentOPen, CommentClose }) {
@@ -24,9 +22,11 @@ export default function BasicCommentMODAL({ commentOPen, CommentClose }) {
                     open={commentOPen}
                     onClose={CommentClose}
                     aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description">
+                    aria-describedby="modal-modal-description " className="comment-modalclass">
 
-                    <Box sx={style}>
+
+                    <Box sx={style} className="comment-boxModal">
+                        <h1>Comment Section <hr className="comment-sec-hr" /></h1>
 
                     </Box>
 
