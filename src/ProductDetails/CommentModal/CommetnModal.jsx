ec357ed735780fae607ffd05701603ba73f020d3
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-10-23 16:49:09 +0530

    add : commentModal.jsx

--- src/ProductDetails/CommentModal/CommetnModal.jsx
+++ src/ProductDetails/CommentModal/CommetnModal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './comment.css';
-import { TopBarImage4 } from "../../utils";
+import { TopBarImage1, TopBarImage2, TopBarImage4, TopBarImage7 } from "../../utils";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 const CommentBox = () => {
 
@@ -16,45 +17,55 @@ const CommentBox = () => {
                             <div className="user-img">
                                 <img src={TopBarImage4} alt="user-img" />
                                 <p>Lorem Ipsum has been the industry's ...</p>
+                                <BsThreeDotsVertical className="cmnt-edits" />
                             </div>
                         </div>
                     </div>
                 </div>
-
+                <hr className="coment-horizontalLine" />
                 <div className="comments-list">
                     <div className="commet-col">
                         <div className="user-coment-selection">
                             <div className="user-img">
-                                <img src={TopBarImage4} alt="user-img" />
+                                <img src={TopBarImage2} alt="user-img" />
                                 <p>Lorem Ipsum has been the industry's ...</p>
+                                <BsThreeDotsVertical className="cmnt-edits" />
                             </div>
                         </div>
                     </div>
                 </div>
+                <hr className="coment-horizontalLine" />
                 <div className="comments-list">
                     <div className="commet-col">
                         <div className="user-coment-selection">
                             <div className="user-img">
-                                <img src={TopBarImage4} alt="user-img" />
+                                <img src={TopBarImage7} alt="user-img" />
                                 <p>Lorem Ipsum has been the industry's ...</p>
+                                <BsThreeDotsVertical className="cmnt-edits" />
                             </div>
                         </div>
                     </div>
                 </div>
+                <hr className="coment-horizontalLine" />
                 <div className="comments-list">
                     <div className="commet-col">
                         <div className="user-coment-selection">
                             <div className="user-img">
-                                <img src={TopBarImage4} alt="user-img" />
+                                <img src={TopBarImage1} alt="user-img" />
                                 <p>Lorem Ipsum has been the industry's ...</p>
+                                <BsThreeDotsVertical className="cmnt-edits" />
                             </div>
                         </div>
                     </div>
                 </div>
+                  <hr className="coment-horizontalLine" />
 
-
+                <form>
+                    <textarea placeholder="Add your comment" className="txt-wrp"/>
+                </form>
             </div>
         </>
+
     )
 }
 export default CommentBox;
