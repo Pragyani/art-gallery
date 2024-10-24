20269e8e839fd43f16e3a45bd0e518f890f6b843
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-10-24 15:24:27 +0530

    add : changes & updation  in commentModal.js

--- src/ProductDetails/CommentModal/CommetnModal.jsx
+++ src/ProductDetails/CommentModal/CommetnModal.jsx
@@ -1,50 +1,57 @@
 import React, { useState } from "react";
 import './comment.css';
-import { TopBarImage1, TopBarImage2, TopBarImage4, TopBarImage7 } from "../../utils";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import { IoClose } from "react-icons/io5";
+import { BsFillSendFill } from "react-icons/bs";
 
-const CommentBox = ({ postComment }) => {
-    console.log(postComment);
+const CommentBox = ({ postComment, onClose }) => {
+    const [newComment, setNewComment] = useState("");
 
-    const [newComment, setNewComment] = useState();
-    console.log(newComment)
-
-    const handleCommentSubmmit = (e) => {
+    const handleCommentSubmit = (e) => {
         e.preventDefault();
-    }
+        if (newComment.trim()) {
+            const commentData = {
+                user: 'Your Comment',
+                comment: newComment,
+            };
+            postComment.reviews.push(commentData);
+            setNewComment("");
+        }
+    };
+
+    const reviews = postComment?.reviews || [];
+
     return (
-        <>
-            <div className="comment-section">
-                <h2>Comments</h2>
-                <hr />
+        <div className="comment-section">
+            <h2>Comments for {postComment?.name || "Post"}</h2>
+            <span onClick={onClose}><IoClose /></span>
+            <hr />
 
-                <div className="comments-list">
-                    {postComment.map((element) => (
-                        <div key={element.id}>
-                            {element.reviews.map((review, index) => (
-                                <div className="commet-col" key={index}>
-                                    <div className="user-coment-selection">
-                                        <div className="user-img">
-                                            <img src={element.image} alt="user-img" />
-                                            <p><strong>{review.user}</strong>: {review.comment}</p>
-                                            <BsThreeDotsVertical className="cmnt-edits" />
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
+            <div className="comments-list">
+                {reviews.map((review, index) => (
+                    <div className="commet-col" key={index}>
+                        <div className="user-coment-selection">
+                            <div className="user-img">
+                                <p><strong>{review.user}</strong>: {review.comment}</p>
+                                <BsThreeDotsVertical className="cmnt-edits" />
+                            </div>
                         </div>
-                    ))}
+                        <hr className="coment-horizontalLine" />
+                    </div>
 
-
-                </div>
-                <hr className="coment-horizontalLine" />
-                <form onSubmit={handleCommentSubmmit}>
-                    <textarea placeholder="Add your comment" value={newComment} onChange={(e) => setNewComment(e.target.value)} className="txt-wrp" />
-                    <button type="submit">Submit</button>
-                </form>
+                ))}
             </div>
-        </>
 
-    )
-}
+            <form onSubmit={handleCommentSubmit}>
+                <textarea
+                    placeholder="Add your comment"
+                    value={newComment}
+                    onChange={(e) => setNewComment(e.target.value)}
+                    className="txt-wrp"
+                />
+                <span type="submit" className="sent-box"><BsFillSendFill /></span>
+            </form>
+        </div>
+    );
+};
 export default CommentBox;
