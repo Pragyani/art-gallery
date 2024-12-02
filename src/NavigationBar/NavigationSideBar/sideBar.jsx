90a02a88e0974fe9b47a6d80ef1c5e0e1c361210
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-12-02 19:03:52 +0530

    add : changes in sideBar.jsx

--- src/NavigationBar/NavigationSideBar/sideBar.jsx
+++ src/NavigationBar/NavigationSideBar/sideBar.jsx
@@ -6,9 +6,9 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useSelector } from "react-redux";
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import TurnedInRoundedIcon from '@mui/icons-material/TurnedInRounded';
-import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import SideTopicBar from "./Sidetopicbar/SideTopicBar";
 import Button from '@mui/material/Button';
+import { GoHeartFill } from "react-icons/go";
 
 const SideBar = () => {
     const cartItems = useSelector(state => state.cartProducts);
@@ -21,7 +21,7 @@ const SideBar = () => {
                         <div className="category-list">
                             <ul>
                                 <Link to={'/'}><li><HomeRoundedIcon className="user-prof-i" /> Home</li></Link>
-                                <Link to={'/popular-page'}><li><TrendingUpIcon className="user-prof-i" /> Popular Post</li></Link>
+                                <Link to={'/popular-page'}><li><GoHeartFill className="user-prof-i" /> Liked Post</li></Link>
                                 <hr />
                                 <div className="matrl-list">
                                     <Link to={'/saved-post'}><li><TurnedInRoundedIcon className="user-prof-i" /> Saved Post</li></Link>
@@ -29,7 +29,7 @@ const SideBar = () => {
                                     <Link to={'/add-to-cart'}><li><ShoppingCartIcon className="user-prof-i" />Your Cart <Button variant="text" className="crt-btn">{cartItems.length}</Button></li></Link>
                                 </div>
                                 <hr />
-                                
+
                                 <SideTopicBar />
                             </ul>
                         </div>
