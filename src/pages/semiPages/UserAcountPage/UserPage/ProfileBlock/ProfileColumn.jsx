4a28977ffb500dcc3a79924d49ad56c873c6ae2b
PRAGYA PANDEY <pragyani2004@gmail.com>
2025-01-23 23:35:09 +0530

    removed: div's from the ProfileColumn.jsx

--- src/pages/semiPages/UserAcountPage/UserPage/ProfileBlock/ProfileColumn.jsx
+++ src/pages/semiPages/UserAcountPage/UserPage/ProfileBlock/ProfileColumn.jsx
@@ -8,26 +8,22 @@ const ProfileColumn = () => {
             <div className="column-box">
                 <div className="column-mt-1">
                     <div className="colun-flexbox">
-                        <div className="user-block">
-                            <div className="text-file">
-                                <div className="user-pImg">
-                                    <img src={UserIMAGE} alt="image" />
-                                </div>
-                            </div>
-                        </div>
+
+                        <img src={UserIMAGE} alt="image" />
+
 
                         {/* <hr className="column-hr"/> */}
-                        
+
                         <div className="user-data">
                             <span className="brnd-name">BRAND NAME : <h1>PragyAni Gallery</h1></span>
-                            <hr className="col-hr"/>
+                            <hr className="col-hr" />
                             <span className="user-id">user id: <h3>@anipragss</h3></span>
-                            <hr className="col-hr"/>
+                            <hr className="col-hr" />
                         </div>
                     </div>
                 </div>
             </div>
         </>
     )
-} 
+}
 export default ProfileColumn;
